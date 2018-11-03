8d308d9ae0457aa4ab1c7912c0a202f585d61b8d
Yeo0 <wooli6@naver.com>
2018-11-03 12:03:39 +0900

    upload_bmi_tensorboard

--- tensorboard2.py
+++ tensorboard2.py
@@ -1,6 +1,28 @@
 #Tensorboard 보기 쉽게 정리하기
 #플레이스 홀더로 이름 붙이기
+import pandas as pd
+import numpy as np
 import tensorflow as tf
+
+#csv 파일 로드
+data=pd.read_csv("bmi.csv")
+
+
+#정규화
+data["height"]=data["height"]/200
+data["weight"]=data["weight"]/100
+
+#레이블을 배열로 변환하기
+#- thin=(1,0,0) / normar=(0,1,0) / fat=(0,0,1)
+bclass={"thin":[1,0,0],"normal":[0,1,0],"fat":[0,0,1]}
+data["label_pat"]=data["label"].apply(lambda x: np.array(bclass[x]))
+
+#테스트를 위한 데이터 분류 
+test_csv=data[15000:20000]
+test_pat=test_csv[["weight", "height"]]
+test_ans=list(test_csv["label_pat"])
+
+#데이터 플레이스
 
 x=tf.placeholder(tf.float32, [None, 2], name="x")
 y_=tf.placeholder(tf.float32, [None, 3], name="y_")
@@ -24,9 +46,36 @@ with tf.name_scope('loss') as scope:
 #training계산을 scope로 묶기
 with tf.name_scope('training') as scope:
     optimizer=tf.train.GradientDescentOptimizer(0.01)
-    trian=optimizer.minimize(cross_entropy)
+    train=optimizer.minimize(cross_entropy)
     
 #accuracy계산을 scope로 묶기
 with tf.name_scope('accuracy') as scope:
     predict=tf.equal(tf.argmax(y,1), tf.argmax(y_,1))
     accuracy=tf.reduce_mean(tf.cast(predict, tf.float32))
+
+#세션 생성
+sess=tf.Session()
+
+#tensorboard사용
+tw=tf.summary.FileWriter("log_dir", graph=sess.graph)
+
+#세션 실행
+sess.run(tf.global_variables_initializer()) #변수 초기화
+
+#학습시키기
+for step in range(3500):
+    i = (step*100)%14000 #14000과 나눴을 때의 나머지
+    rows=data[1+i:1+i+100]
+    x_pat=rows[["weight","height"]]
+    y_ans=list(rows["label_pat"])
+    fd={x:x_pat, y_:y_ans}
+    sess.run(train, feed_dict=fd)
+    if step%500==0:
+        ce=sess.run(cross_entropy, feed_dict=fd)            
+        acc=sess.run(accuracy, feed_dict={x:test_pat, y_:test_ans})
+        print("step=", step, "cre=", ce, "acc=", acc)
+
+
+#최종적인 정답률
+acc=sess.run(accuracy, feed_dict={x:test_pat, y_:test_ans})
+print("정답률=",acc)
